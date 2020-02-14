039551b1ae0f21a45ee36376291eb52d86701316
Abbah <anohabbah@gmail.com>
2020-02-14 13:53:14 +0100

    feat(fingerprint): read browser fingerprint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as Fingerprint from 'fingerprintjs2';
+import map from 'lodash/fp/map';
+import join from 'lodash/fp/join';
+import pipe from 'lodash/fp/pipe';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  fingerprint: string;
+  time;
+  details: string;
+
+  async ngOnInit(): Promise<void> {
+    const d1: Date = new Date();
+    const components = await Fingerprint.getPromise();
+    this.fingerprint = Fingerprint.x64hash128(pipe(map('value'), join())(components));
+    const d2 = new Date();
+    // @ts-ignore
+    this.time = (d2 - d1);
+
+    this.details = pipe(
+      map(({ key, value }) => `${key} = ${String(value).substr(0, 100)}`),
+      join('\n')
+    )(components);
+  }
 }
