110cd3a3adf91d1c244d3b07942083d3d5fda0bd
Abbah <anohabbah@gmail.com>
2020-02-15 13:41:46 +0100

    improvement(routing): add angular routing support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import * as Fingerprint from 'fingerprintjs2';
-import map from 'lodash/fp/map';
-import join from 'lodash/fp/join';
-import pipe from 'lodash/fp/pipe';
-import {ApiService} from './api.service';
-import {Connection} from './connection';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  fingerprint: string;
-  time;
-  details: string;
-  components: { key: string, value: any }[];
-
-  constructor(private apiService: ApiService) {}
-
-  async ngOnInit(): Promise<void> {
-    const d1: Date = new Date();
-    const components = await Fingerprint.getPromise();
-    const fingerprint = Fingerprint.x64hash128(pipe(map('value'), join(''))(components));
-    this.apiService
-      .apply(fingerprint, components)
-      .subscribe((res: Connection) => {
-        this.fingerprint = res._id;
-        const d2 = new Date();
-        // @ts-ignore
-        this.time = (d2 - d1);
-
-        this.components = res.fingerprint;
-      });
-  }
+export class AppComponent {
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { TrackingComponent } from './tracking/tracking.component';
+
+const routes = [
+  { path: '', component: TrackingComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackingComponent
   ],
   imports: [
+    RouterModule.forRoot(routes, { enableTracing: true }),
     BrowserModule,
     HttpClientModule
   ],
