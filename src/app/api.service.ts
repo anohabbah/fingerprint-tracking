29a02f8c106128a1b283ba174c5d933898aca8b5
Abbah <anohabbah@gmail.com>
2020-02-15 16:59:56 +0100

    improvement(api.service.ts): add error handler

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, of} from 'rxjs';
 import { Connection } from './connection';
-import {map} from 'rxjs/operators';
+import {catchError, map} from 'rxjs/operators';
+import {error} from 'winston';
 
 @Injectable({
   providedIn: 'root'
@@ -19,54 +20,91 @@ export class ApiService {
   getSystemStats(): Observable<object> {
     return this.http
       .get(`${this.statisticsBaseURL}/systems`)
-      .pipe(map((res: { data: object }) => res.data));
+      .pipe(
+        map((res: { data: object }) => res.data),
+        catchError(this.handleError('getSystemStats', {}))
+      );
   }
 
   getGraphicCardStats(): Observable<object> {
     return this.http
       .get(`${this.statisticsBaseURL}/graphic-cards`)
-      .pipe(map((res: { data: object }) => res.data));
+      .pipe(
+        map((res: { data: object }) => res.data),
+        catchError(this.handleError('getGraphicCardStats', {}))
+      );
   }
 
   getTimezoneStats(): Observable<object> {
     return this.http
       .get(`${this.statisticsBaseURL}/timezones`)
-      .pipe(map((res: { data: object }) => res.data));
+      .pipe(
+        map((res: { data: object }) => res.data),
+        catchError(this.handleError('getTimezoneStats', {}))
+      );
   }
 
   getDeviceMemoryStats(): Observable<object> {
     return this.http
       .get(`${this.statisticsBaseURL}/momeries`)
-      .pipe(map((res: { data: object }) => res.data));
+      .pipe(
+        map((res: { data: object }) => res.data),
+        catchError(this.handleError('getDeviceMemoryStats', {}))
+      );
   }
 
   getHardwareConcurrencyStats(): Observable<object> {
     return this.http
       .get(`${this.statisticsBaseURL}/hardware-concurrency`)
-      .pipe(map((res: { data: object }) => res.data));
+      .pipe(
+        map((res: { data: object }) => res.data),
+        catchError(this.handleError('getHardwareConcurrencyStats', {}))
+      );
   }
 
   getBrowserLanguageStats(): Observable<object> {
     return this.http
       .get(`${this.statisticsBaseURL}/languages`)
-      .pipe(map((res: { data: object }) => res.data));
+      .pipe(
+        map((res: { data: object }) => res.data),
+        catchError(this.handleError('getBrowserLanguageStats', {}))
+      );
   }
 
   getBrowserStats(): Observable<object> {
     return this.http
       .get(`${this.statisticsBaseURL}/user-agents`)
-      .pipe(map((res: { data: object }) => res.data));
+      .pipe(
+        map((res: { data: object }) => res.data),
+        catchError(this.handleError('getBrowserStats', {}))
+      );
   }
 
   getUserStats(): Observable<object> {
     return this.http
       .get(`${this.statisticsBaseURL}/users`)
-      .pipe(map((res: { data: object }) => res.data));
+      .pipe(
+        map((res: { data: object }) => res.data),
+        catchError(this.handleError('getUserStats', {}))
+      );
   }
 
   getMonthlyConnectionStats(): Observable<object> {
     return this.http
       .get(`${this.statisticsBaseURL}/monthly`)
-      .pipe(map((res: { data: object }) => res.data));
+      .pipe(
+        map((res: { data: object }) => res.data),
+        catchError(this.handleError('getMonthlyConnectionStats', {}))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (err: any): Observable<T> => {
+      console.error(err);
+
+      // this.log(`${operation} failed: ${err.message}`);
+
+      return of(result as T);
+    };
   }
 }
