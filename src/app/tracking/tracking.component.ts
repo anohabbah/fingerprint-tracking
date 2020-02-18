fb535c0d06eec09ef3f05d20af4a8e031c64e45e
Abbah <anohabbah@gmail.com>
2020-02-18 08:22:33 +0100

    improvement(fingerprint hash): make it more unique

--- src/app/tracking/tracking.component.ts
+++ src/app/tracking/tracking.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as Fingerprint from 'fingerprintjs2';
 import map from 'lodash/fp/map';
 import join from 'lodash/fp/join';
+import filter from 'lodash/fp/filter';
 import pipe from 'lodash/fp/pipe';
 import { ApiService } from '../api.service';
 import { Connection } from '../connection';
@@ -14,7 +15,9 @@ import { Connection } from '../connection';
 export class TrackingComponent implements OnInit {
   fingerprint: string;
   time;
-  details: string;
+  relevantKeys: string[] = ['userAgent', 'language', 'colorDepth', 'deviceMemory',
+    'hardwareConcurrency', 'screenResolution', 'availableResolution', 'timezone',
+    'platform', 'webglVendorAndRenderer', 'touchSupport'];
   components: { key: string, value: any }[];
 
   constructor(private apiService: ApiService) {}
@@ -22,7 +25,8 @@ export class TrackingComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     const d1: Date = new Date();
     const components = await Fingerprint.getPromise();
-    const fingerprint = Fingerprint.x64hash128(pipe(map('value'), join(''))(components));
+    const valuesStr = pipe(filter(({ key }) => this.relevantKeys.includes(key)), map('value'), join(''))(components);
+    const fingerprint = Fingerprint.x64hash128(valuesStr);
     this.apiService
       .apply(fingerprint, components)
       .subscribe((res: Connection) => {
